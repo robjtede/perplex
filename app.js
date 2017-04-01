8917bcd5310dd956144cb532224567d7e4603be5
Kai Barclay <hbarclay1@sheffield.ac.uk>
2017-04-01 19:32:02 +0100

    scroll puzzle

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-/* global Game:false MousePuzzle:false */
+/* global Game:false MousePuzzle:false ScrollPuzzle:false*/
 'use strict';
 
 const game = new Game();
 
 let id = 1;
 const mousePuzzle = new MousePuzzle(id++, game);
+const scrollPuzzle = new ScrollPuzzle(id++, game);
 
 // this will be replaced by event handlers
 // when each puzzle is selected. only activated
 // puzzles can be completed
 mousePuzzle.activate();
+scrollPuzzle.activate();
