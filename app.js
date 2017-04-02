f088e893580c9dc313fc87776d8d3aec29b932ff
Rob Ede <robjtede@icloud.com>
2017-04-02 04:08:25 +0100

    add offline puzzle

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-/* global Game:false MousePuzzle:false FullscreenPuzzle:false */
+/* global Game:false MousePuzzle:false FullscreenPuzzle:false OfflinePuzzle:false */
 'use strict';
 
 const game = new Game();
 
 let id = 1;
+
 const mousePuzzle = new MousePuzzle(id++, game);
 const fullScreenPuzzle = new FullscreenPuzzle(id++, game);
+const offlinePuzzle = new OfflinePuzzle(id++, game);
 
 // this will be replaced by event handlers
 // when each puzzle is selected. only activated
 // puzzles can be completed
 mousePuzzle.activate();
 fullScreenPuzzle.activate();
+offlinePuzzle.activate();
 
 document.addEventListener('DOMContentLoaded', () => {
   game.start();
