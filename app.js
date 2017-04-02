bcf4eb0a79c24e27309b8467d8ebbff689a987fe
Kai Barclay <hbarclay1@sheffield.ac.uk>
2017-04-02 08:33:39 +0100

    almost complete clipboard puzzle
    needs paste field

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-/* global Game:false MousePuzzle:false */
+/* global Game:false MousePuzzle:false ClipboardPuzzle:false*/
 'use strict';
 
 const game = new Game();
 
 let id = 1;
 const mousePuzzle = new MousePuzzle(id++, game);
+const clipPuzzle = new ClipboardPuzzle(id++, game);
 
 // this will be replaced by event handlers
 // when each puzzle is selected. only activated
 // puzzles can be completed
 mousePuzzle.activate();
+clipPuzzle.activate();
