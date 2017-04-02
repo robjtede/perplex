b78996001e2c600b444735fa8cf3c361b93f9d0b
Kai Barclay <hbarclay1@sheffield.ac.uk>
2017-04-02 10:57:53 +0100

    increased metaness

--- puzzles/NotificationPuzzle.js
+++ puzzles/NotificationPuzzle.js
@@ -11,16 +11,40 @@ class NotificationPuzzle extends Puzzle {
       .requestPermission()
       .then(result => {
         this.granted = window.Notification.permission === 'granted';
-        if (result !== 'granted') throw new Error('You must allow notification to solve this puzzle.');
+        if (result !== 'granted') throw new Error('You must allow notifications to solve this puzzle.');
       });
   }
 
   onActivate () {
     if (this.granted) {
-      const n = new Notification('title');
-      n.addEventListener('click', ev => {
-        n.close();
-        this.complete();
+      const n1 = new Notification('hey there');
+      n1.addEventListener('click', ev => {
+        n1.close();
+        const n2 = new Notification('you\'re getting good at this');
+        n2.addEventListener('click', ev => {
+          n2.close();
+          const n3 = new Notification('you deserve a cookie');
+          n3.addEventListener('click', ev => {
+            n3.close();
+            const n4 = new Notification('have a cookie 🍪');
+            n4.addEventListener('click', ev => {
+              n4.close();
+              const n5 = new Notification('oh, you wanted the solution instead?');
+              n5.addEventListener('click', ev => {
+                n5.close();
+                this.complete();
+                const n6 = new Notification('there you go. happy?');
+                n6.addEventListener('click', ev => {
+                  n6.close();
+                  const n7 = new Notification('good. happy puzzling!');
+                  n7.addEventListener('click', ev => {
+                    n7.close();
+                  });
+                });
+              });
+            });
+          });
+        });
       });
     }
   }
